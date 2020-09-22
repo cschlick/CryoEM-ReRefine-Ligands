3b3a8605d84df202fbd2429aecd7ca414881f8c6
cschlick <cjschlicksup@lbl.gov>
2020-09-21 18:38:45 -0700

    updates to handle models without ligands

--- ligand_map_model_validation.py
+++ ligand_map_model_validation.py
@@ -98,6 +98,11 @@ def ligands_map_model_cc(entry):
 	"""
 	model_path = entry.model_file
 	map_path = entry.map_file
+	if model_path is None:
+		model_path = entry.local_model_file
+	if map_path is None:
+		map_path = entry.local_map_file
+
 
 	if not all([os.path.exists(model_path),os.path.exists(map_path)]):
 		logging.info("Skipping entry because file paths do not exist: "+str(entry))
@@ -206,10 +211,10 @@ def process_directory(input_directory,nproc=2,output_directory=None,
 			group_args.add(key="entry_id", value=entry_id)
 			group_args.add(key="pdb_accession", value=entry_id[:4])
 			group_args.add(key="emdb_accession", value=entry_id[5:])
-			# group_args.add(key="local_map_file", value=os.path.join(entry_path,
-			# 																									 entry_id+".map"))
-			# group_args.add(key="local_model_file", value=os.path.join(entry_path,
-			# 																									 entry_id+".cif"))
+			group_args.add(key="local_map_file", value=os.path.join(entry_path,
+																												 entry_id+".map"))
+			group_args.add(key="local_model_file", value=os.path.join(entry_path,
+																												 entry_id+".cif"))
 			if output_directory is not None:
 				group_args.add(key="output_directory",value=output_directory)
 			if (output_directory is not None and entry_id in output_contents) and \
