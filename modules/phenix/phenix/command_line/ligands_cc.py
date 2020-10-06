995378d49f8c89f789e2ce87e5d6ffbe068c28d7
Christopher Schlicksup <cjschlicksup@lbl.gov>
2020-10-06 13:00:43 -0700

    update files

--- modules/phenix/phenix/command_line/ligands_cc.py
+++
@@ -1,8 +0,0 @@
-# LIBTBX_SET_DISPATCHER_NAME phenix.ligands_cc.py
-from __future__ import absolute_import, division, print_function
-
-from iotbx.cli_parser import run_program
-from phenix.programs import ligands_cc
-
-if __name__ == '__main__':
-  run_program(program_class=ligands_cc.Program)
